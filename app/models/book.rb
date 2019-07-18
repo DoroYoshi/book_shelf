b490c72d3adb0db2fcaf0bb3eb167866aa4e60bc
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-07-18 11:50:46 +0900

    国際化対応と画像の登録機能まで完成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_one_attached :image
   validates :title, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :publish_date, presence: true
